7911df99bbcad4b9b018a35f2fb821402fbed567
csc <csc@codemanufaktur.com>
2019-12-14 15:50:56 +0100

    API-express.js & ELasticsearch client. Elasticclient not functioning currently

--- rest-api.js
+++ rest-api.js
@@ -1,7 +1,27 @@
-// const http = require('http');
-//
-// const server = http.createServer((req, res) => {
-//   res.end('Response');
-// });
-// console.log('Hello');
 const express = require('express');
+const router = express.Router();
+const app = express();
+const elasticSearch = require('elasticsearch');
+const elasticClient = new elasticSearch.Client({
+  host: 'http://localhost:9200',
+  log: 'trace'
+});
+console.log(elasticClient);
+
+
+
+app.use((req, res, next) => {
+  console.log('Header Modification');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  next();
+});
+
+app.use('/api/shakespeare', async(req, res, next)  => {
+  console.log('Api Call. Elastic Client = ', elasticClient);
+  const response = await elasticClient.search({index: 'shakespeare', body: req.body});
+  // res.send(response);
+});
+
+module.exports = app;
--- src/app/searchbar/searchbar.component.ts
+++ src/app/searchbar/searchbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {debounce} from 'rxjs/operators';
 
 @Component({
   selector: 'app-searchbar',
@@ -7,14 +9,42 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./searchbar.component.css']
 })
 export class SearchbarComponent implements OnInit {
+  inputValue: string;
+  debounceRetrieval;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.debounceRetrieval = this.debounce(this.retrieveDocsFromElastic, 1000);
+}
 
   ngOnInit() {
+    document.getElementById('input_field')
+      .addEventListener('keypress', (event) => {
+        this.inputValue = (document.getElementById('input_field') as HTMLInputElement).value;
+        console.log('Input value: ', this.inputValue);
+        this.debounceRetrieval(this.inputValue);
+      });
   }
 
-  onClickGetDocuments(form: NgForm) {
-
+  retrieveDocsFromElastic() {
+    console.log('After debounce function');
+    this.http.post('http://localhost:3000/api/shakespeare', {queryValue : this.inputValue})
+      .subscribe((response) => {
+        console.log(response);
+      });
   }
 
+  // Returns a function, that, as long as it continues to be invoked, will not
+  // be triggered. The function will be called after it stops being called for
+  // N milliseconds. If `immediate` is passed, trigger the function on the
+  // leading edge, instead of the trailing.
+  debounce(cb, interval) {
+    // console.log('In debounce function: ', cb, interval, immediate);
+    let timeout;
+    return function() {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+      timeout = setTimeout(() => cb.apply(this, [...arguments]), interval);
+    };
+  }
 }
